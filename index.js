262265702e70f95dd14dd28bf12a455dfa63381d
MuliaHartawan <mulia.hartawan011@gmail.com>
2022-04-26 12:28:53 +0700

    query get all books

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const {
+    ApolloServer
+} = require('apollo-server');
 const mongoose = require('mongoose');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 //connection to mongodb
-mongoose.connect('mongodb://localhost:27017/learn-graphql', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true
-})
+mongoose.connect('mongodb://localhost:27017/learn-graphql');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen()
+    .then(({
+        url
+    }) => {
+        console.log(`Server ready at ${url}`)
+    })
